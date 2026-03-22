33a06b996ad408c09c34f1ddc71c0e2d5dd7890f
Joshua <joshuaodunbaku@gmail.com>
2026-03-22 16:03:11 +0100

    homepage done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Index from './Routes/Index.jsx';
-// import About from './Routes/About.jsx';
-// import Contact from './Routes/Contact.jsx';
-// import Products from './Routes/Products.jsx';
-// import Dashboard from './Routes/Dashboard.jsx';
 import NotFound from './Routes/NotFound.jsx';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Index />} />
-      {/* <Route path="/about" element={<About />} /> */}
-      {/* <Route path="/contact" element={<Contact />} /> */}
-      {/* <Route path="/products" element={<Products />} /> */}
-      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
