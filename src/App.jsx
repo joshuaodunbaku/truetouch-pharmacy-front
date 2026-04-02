b36b0c47063f497f8ea1ce45b52813c99fe812bc
Joshua <joshuaodunbaku@gmail.com>
2026-04-02 20:09:27 +0100

    Added Contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Index from './Routes/Index.jsx';
 // import About from './Routes/About.jsx';
-// import Contact from './Routes/Contact.jsx';
+import Contact from './Routes/Contact.jsx';
 import Products from './Routes/Products.jsx';
 import Dashboard from './Routes/Dashboard.jsx';
 import NotFound from './Routes/NotFound.jsx';
@@ -12,6 +12,7 @@ const App = () => (
       <Route path="/" element={<Index />} />
       <Route path="/products" element={<Products />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/contact" element={<Contact />} />
       {/* <Route path="/about" element={<About />} /> */}
       <Route path="*" element={<NotFound />} />
     </Routes>
